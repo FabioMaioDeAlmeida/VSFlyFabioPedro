f618105f28be3d705509b2ba21c19030c120331c
Pedro <pedro.saraivaf@hes-so.ch>
2022-10-22 11:27:22 +0200

    Commit

---
+++ VSFlyFabioPedro/VSFlyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSFlyFabioPedro
+{
+    internal class VSFlyContext
+    {
+        public static string ConnectionString { get; set; } = @"Data Source=153.109.124.35;Initial Catalog=VSFlyFabioPedro;Integrated Security=False;User Id=6231db;Password=Pwd46231.;MultipleActiveResultSets=True";
+
+    }
+}
